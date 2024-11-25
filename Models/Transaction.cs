e883ddeb01d36a84e81bf7a163575ca7851a77a4
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-25 15:06:07 +0400

    create Transaction table

---
+++ Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLab1.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string SourceAccNumber { get; set; }  // This is the account number for the source account
+
+        [Required]
+        public string Operation { get; set; }  // Could be 'Deposit' or 'Withdraw'
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        // Foreign key to BankAccount
+        public int BankAccountId { get; set; }
+
+        // Navigation property
+        public BankAccount BankAccount { get; set; }
+    }
+}
