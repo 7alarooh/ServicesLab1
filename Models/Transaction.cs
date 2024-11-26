3144d09714fbc8fa200b89b7d1f9555565b0c9ed
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-26 15:31:19 +0400

    Update the Transaction Model

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +14,19 @@ namespace ServicesLab1.Models
         public int Id { get; set; }
 
         [Required]
-        public string SourceAccNumber { get; set; }  // This is the account number for the source account
+        public int AccountId { get; set; } // Foreign Key to BankAccount
 
         [Required]
-        public string Operation { get; set; }  // Could be 'Deposit' or 'Withdraw'
+        [StringLength(20)]
+        public string Operation { get; set; } // Deposit, Withdraw, Transfer
 
         [Required]
         public decimal Amount { get; set; }
 
-        // Foreign key to BankAccount
-        public int BankAccountId { get; set; }
+        [StringLength(50)]
+        public string SourceAccNumber { get; set; } // "ATM", source account, or "None"
 
-        // Navigation property
-        public BankAccount BankAccount { get; set; }
+        [ForeignKey(nameof(AccountId))]
+        public BankAccount BankAccount { get; set; } // Navigation Property
     }
 }
